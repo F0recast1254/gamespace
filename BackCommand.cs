99c264ab3f645c5e268d0c16c1b043107a3d4237
F0recast1254 <95315760+F0recast1254@users.noreply.github.com>
2023-11-19 01:25:33 -0500

    Add files via upload

---
+++ BackCommand.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Template
+{
+    public class BackCommand : Command
+    {
+        public BackCommand() : base()
+        {
+            this.Name = "back";
+        }
+
+        override
+        public bool Execute(Player player)
+        {
+                player.Back();
+            return false;
+        }
+    }
+}
